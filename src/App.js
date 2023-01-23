cee7671e8bc55e84fc5ce63093effb02d50cc4c1
NOURZAIED <zaiednour7@gmail.com>
2023-01-23 12:37:13 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import {useEffect, useState } from 'react';
+import { Card } from 'react-bootstrap';
+import axios from 'axios';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const [users, setUsers]= useState ([]);
+  const getDataUsers = async () => {
+    try {
+      const {data} = await axios.get('https://jsonplaceholder.typicode.com/')
+      setUsers(data)
+    } catch (error) {
+      console.log(error); 
+      }
+      
+    }
+    useEffect(() => {
+     getDataUsers();
+    }, []);
+    console.log(users);
+      return (
+        <div className="App">
+          {users.map((el) => (
+            <Card key={el.id} style={{width: "18rem"}}>
+              <Card.Body>
+                <Card.Title>{el.name}</Card.Title>
+                <Card.Subtitle className="mb-2 text-muted">
+                  {el.email}
+                </Card.Subtitle>
+                <Card.Text>
+                  {el.phone}
+                </Card.Text>
+                <Card.Text>
+                  {el.website}
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          ))}
+          
+        </div>
+      );
+    }
 
 export default App;
